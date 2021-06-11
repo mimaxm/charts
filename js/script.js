a494928f7d974236ccda9a0106f4d9e21fce9a0f
Nikita <nikita.tolkach@ramax.ru>
2021-06-11 18:37:31 +0300

    asd

--- js/script.js
+++ js/script.js
@@ -1,217 +1,220 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let profitValue = document.querySelector('.strategy-profit__value')
-    let profitText = document.querySelector('.strategy-profit__text')
-    function addText(range, e) {
-    let summ = 0
-    
-    range.map(el => {
-        summ += el
-    })
-    
-    profitValue.innerHTML = `${(summ/range.length).toFixed(2)}%`;
-    profitText.innerHTML = `<div>Средняя доходность за выбранный период </br> в долларах США, с учётом издержек</div>`;
-}
 
-const idAll = document.querySelectorAll('.chart-container');
+    function addText(range, element) {        
+        let wrap = element.parentElement
+        
+        let profitValue = wrap.querySelector('.strategy-profit__value')
+        let profitText = wrap.querySelector('.strategy-profit__text')
+        let summ = 0
+        
+        range.map(el => {
+            summ += el
+        })
+        
+        profitValue.innerHTML = `${(summ/range.length).toFixed(2)}%`;
+        profitText.innerHTML = `<div>Средняя доходность за выбранный период </br> в долларах США, с учётом издержек</div>`;
+    }
+
+    const idAll = document.querySelectorAll('.chart-container');
     
     idAll.forEach(chartId => {
-    console.log(chartId.dataset.url);
-    Highcharts.getJSON(chartId.dataset.url, function (data) {
+        console.log(chartId.dataset.url);
+        Highcharts.getJSON(chartId.dataset.url, function (data) {
 
         // Create the chart
-        Highcharts.setOptions({
-            lang: {
-                rangeSelectorZoom: '',
-            },
-        });
-        
-        let chart = Highcharts.stockChart(chartId.id, {
-            chart: {
-                styledMode: true,
-            },
-            
-            rangeSelector: {
-                selected: 8,
-                inputEnabled: false,
-                labelStyle: {
-                    display: 'none',
+            Highcharts.setOptions({
+                lang: {
+                    rangeSelectorZoom: '',
                 },
-                buttonTheme: {
-                    width: '100%',
-                    fill: 'none',
-                    style: {
-                        color: '#9598a7',
-                        fontWeight: '700',
+            });
+            
+            let chart = Highcharts.stockChart(chartId.id, {
+                chart: {
+                    styledMode: true,
+                },
+                
+                rangeSelector: {
+                    selected: 8,
+                    inputEnabled: false,
+                    labelStyle: {
+                        display: 'none',
                     },
-                    states: {
-                        hover: {
-                            color: '#333',
+                    buttonTheme: {
+                        width: '100%',
+                        fill: 'none',
+                        style: {
+                            color: '#9598a7',
+                            fontWeight: '700',
                         },
-                        select: {
+                        states: {
+                            hover: {
                                 color: '#333',
+                            },
+                            select: {
+                                    color: '#333',
+                            }
                         }
+                    },
+                    buttons: [{
+                        type: 'day',
+                        count: 5,
+                        text: '5 дней',
+                        title: '5 дней',
+                        dataGrouping: {
+                            forced: true,
+                            units: [['day', [1]]]
+                        },
+        
+                    }, {
+                        type: 'month',
+                        count: 1,
+                        text: '1 мес',
+                        title: '1 месяц',
+                    }, {
+                        type: 'month',
+                        count: 3,
+                        text: '3 мес',
+                        title: '3 месяца',
+        
+                    }, {
+                        type: 'month',
+                        count: 6,
+                        text: '6 мес',
+                        title: '6 месяцев',
+                    }, {
+                        type: 'year',
+                        count: 1,
+                        text: '1 год',
+                        title: '1 год',
+                    }, {
+                        type: 'ytd',
+                        text: 'YTD',
+                        title: 'С начала года',
+                    },{
+                        type: 'year',
+                        count: 2,
+                        text: '2 года',
+                        title: '2 года',
+                    },{
+                        type: 'year',
+                        count: 5,
+                        text: '5 лет',
+                        title: '5 лет',
+                    }, {
+                        type: 'all',
+                        text: 'Весь период',
+                        title: 'За всё время',
+                    }]
+                },
+        
+                title: {
+                    text: ''
+                },
+        
+                chart: {
+                    events: {
+                        redraw: () => addText(chart.series[0].processedYData, chartId)
                     }
                 },
-                buttons: [{
-                    type: 'day',
-                    count: 5,
-                    text: '5 дней',
-                    title: '5 дней',
-                    dataGrouping: {
-                        forced: true,
-                        units: [['day', [1]]]
-                    },
-    
-                }, {
-                    type: 'month',
-                    count: 1,
-                    text: '1 мес',
-                    title: '1 месяц',
-                }, {
-                    type: 'month',
-                    count: 3,
-                    text: '3 мес',
-                    title: '3 месяца',
-    
-                }, {
-                    type: 'month',
-                    count: 6,
-                    text: '6 мес',
-                    title: '6 месяцев',
-                }, {
-                    type: 'year',
-                    count: 1,
-                    text: '1 год',
-                    title: '1 год',
-                }, {
-                    type: 'ytd',
-                    text: 'YTD',
-                    title: 'С начала года',
-                },{
-                    type: 'year',
-                    count: 2,
-                    text: '2 года',
-                    title: '2 года',
-                },{
-                    type: 'year',
-                    count: 5,
-                    text: '5 лет',
-                    title: '5 лет',
-                }, {
-                    type: 'all',
-                    text: 'Весь период',
-                    title: 'За всё время',
-                }]
-            },
-    
-            title: {
-                text: ''
-            },
-    
-            chart: {
-                events: {
-                    redraw: () => addText(chart.series[0].processedYData)
-                }
-            },
-    
-            scrollbar: {
-                enabled: false,
-            },
-    
-            navigator: {
-                enabled: false,
-            },
-    
-            exporting: {
-                buttons: {
-                    contextButton: {
-                        enabled: false,
+        
+                scrollbar: {
+                    enabled: false,
+                },
+        
+                navigator: {
+                    enabled: false,
+                },
+        
+                exporting: {
+                    buttons: {
+                        contextButton: {
+                            enabled: false,
+                        },
                     },
                 },
-            },
-    
-            series: [{
-                name: name,
-                data: data,
+        
+                series: [{
+                    name: name,
+                    data: data,
+                    tooltip: {
+                        valueDecimals: 2
+                    },
+                }],
+        
+                legend: {
+                    enabled: true,
+                    align: 'left',
+                },
+        
+                colors: ['#5ecba1', '#50B432', '#ED561B'],
+        
+                xAxis: {
+                    type: "datetime",
+                    crosshair: {
+                        color: "#EBE9EA"
+                    },
+                    gridLineWidth: 1,
+                    gridLineColor: "#EBE9EA",
+                    labels: {
+                        style: {
+                            color: "#9598A7"
+                        },
+                        format: "{value:%d.%m.%Y}"
+                    },
+                    lineColor: "#EBE9EA",
+                    tickLength: 0,
+                    tickWidth: 0
+                },
+        
+                yAxis: [{
+                    labels: {
+                        align: 'left'
+                    },
+                    height: '100%',
+                    resize: {
+                        enabled: true
+                    }
+                }, {
+                    labels: {
+                        align: 'left'
+                    },
+                    top: '80%',
+                    height: '20%',
+                    offset: 0
+                }],
+        
                 tooltip: {
-                    valueDecimals: 2
-                },
-            }],
-    
-            legend: {
-                enabled: true,
-                align: 'left',
-            },
-    
-            colors: ['#5ecba1', '#50B432', '#ED561B'],
-    
-            xAxis: {
-                type: "datetime",
-                crosshair: {
-                    color: "#EBE9EA"
-                },
-                gridLineWidth: 1,
-                gridLineColor: "#EBE9EA",
-                labels: {
-                    style: {
-                        color: "#9598A7"
-                    },
-                    format: "{value:%d.%m.%Y}"
-                },
-                lineColor: "#EBE9EA",
-                tickLength: 0,
-                tickWidth: 0
-            },
-    
-            yAxis: [{
-                labels: {
-                    align: 'left'
-                },
-                height: '100%',
-                resize: {
-                    enabled: true
+                    formatter: function () {
+                        let d = new Date(this.x).customFormat( "#ddd#, #D# #MMMM# #YYYY#" );
+                        return  d  + '<br /><span style="color:#5ecba1;">&#9679;</span> ' + name + ':<b> ' + this.y.toFixed(2) + '%</b>';
+                    }
                 }
-            }, {
-                labels: {
-                    align: 'left'
-                },
-                top: '80%',
-                height: '20%',
-                offset: 0
-            }],
-    
-            tooltip: {
-                formatter: function () {
-                    let d = new Date(this.x).customFormat( "#ddd#, #D# #MMMM# #YYYY#" );
-                    return  d  + '<br /><span style="color:#5ecba1;">&#9679;</span> ' + name + ':<b> ' + this.y.toFixed(2) + '%</b>';
-                }
-            }
+            });
+        
+            Date.prototype.customFormat = function(formatString){
+                var YYYY,YY,MMMM,MMM,MM,M,DDDD,DDD,DD,D,ddd,hhhh,hhh,hh,h,mm,m,ss,s,ampm,AMPM,dMod,th;
+                YY = ((YYYY=this.getFullYear())+"").slice(-2);
+                MM = (M=this.getMonth()+1)<10?('0'+M):M;
+                MMM = (MMMM=["Января","Февраля","Марта","Апреля","Мая","Июня","Июля","Августа","Сентября","Октября","Ноября","Декабря"][M-1]).substring(0,3);
+                DD = (D=this.getDate())<10?('0'+D):D;
+                DDD = (DDDD=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][this.getDay()]).substring(0,3);
+                ddd = (DDDD=["Вс","Пн","Вт","Ср","Чт","Пт","Сб"][this.getDay()]).substring(0,2);
+                th=(D>=10&&D<=20)?'th':((dMod=D%10)==1)?'st':(dMod==2)?'nd':(dMod==3)?'rd':'th';
+                formatString = formatString.replace("#YYYY#",YYYY).replace("#YY#",YY).replace("#MMMM#",MMMM).replace("#MMM#",MMM).replace("#MM#",MM).replace("#M#",M).replace("#DDDD#",DDDD).replace("#DDD#",DDD).replace("#DD#",DD).replace("#D#",D).replace("#ddd#",ddd).replace("#th#",th);
+                h=(hhh=this.getHours());
+                if (h==0) h=24;
+                if (h>12) h-=12;
+                hh = h<10?('0'+h):h;
+                hhhh = hhh<10?('0'+hhh):hhh;
+                AMPM=(ampm=hhh<12?'am':'pm').toUpperCase();
+                mm=(m=this.getMinutes())<10?('0'+m):m;
+                ss=(s=this.getSeconds())<10?('0'+s):s;
+                return formatString.replace("#hhhh#",hhhh).replace("#hhh#",hhh).replace("#hh#",hh).replace("#h#",h).replace("#mm#",mm).replace("#m#",m).replace("#ss#",ss).replace("#s#",s).replace("#ampm#",ampm).replace("#AMPM#",AMPM);
+            };
+        
+            addText(chart.series[0].processedYData, chartId)
         });
-    
-        Date.prototype.customFormat = function(formatString){
-            var YYYY,YY,MMMM,MMM,MM,M,DDDD,DDD,DD,D,ddd,hhhh,hhh,hh,h,mm,m,ss,s,ampm,AMPM,dMod,th;
-            YY = ((YYYY=this.getFullYear())+"").slice(-2);
-            MM = (M=this.getMonth()+1)<10?('0'+M):M;
-            MMM = (MMMM=["Января","Февраля","Марта","Апреля","Мая","Июня","Июля","Августа","Сентября","Октября","Ноября","Декабря"][M-1]).substring(0,3);
-            DD = (D=this.getDate())<10?('0'+D):D;
-            DDD = (DDDD=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][this.getDay()]).substring(0,3);
-            ddd = (DDDD=["Вс","Пн","Вт","Ср","Чт","Пт","Сб"][this.getDay()]).substring(0,2);
-            th=(D>=10&&D<=20)?'th':((dMod=D%10)==1)?'st':(dMod==2)?'nd':(dMod==3)?'rd':'th';
-            formatString = formatString.replace("#YYYY#",YYYY).replace("#YY#",YY).replace("#MMMM#",MMMM).replace("#MMM#",MMM).replace("#MM#",MM).replace("#M#",M).replace("#DDDD#",DDDD).replace("#DDD#",DDD).replace("#DD#",DD).replace("#D#",D).replace("#ddd#",ddd).replace("#th#",th);
-            h=(hhh=this.getHours());
-            if (h==0) h=24;
-            if (h>12) h-=12;
-            hh = h<10?('0'+h):h;
-            hhhh = hhh<10?('0'+hhh):hhh;
-            AMPM=(ampm=hhh<12?'am':'pm').toUpperCase();
-            mm=(m=this.getMinutes())<10?('0'+m):m;
-            ss=(s=this.getSeconds())<10?('0'+s):s;
-            return formatString.replace("#hhhh#",hhhh).replace("#hhh#",hhh).replace("#hh#",hh).replace("#h#",h).replace("#mm#",mm).replace("#m#",m).replace("#ss#",ss).replace("#s#",s).replace("#ampm#",ampm).replace("#AMPM#",AMPM);
-        };
-    
-        addText(chart.series[0].processedYData)
     });
-});
 
 const name = 'Доходность с учётом издержек *';
 
